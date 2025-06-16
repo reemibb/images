fcf83ece216c91f16d6020b992f09250552ed405
Reem Ibrahim <ri102163@net.iul.edu.lb>
2025-06-16 15:52:35 +0300

    Update PdfViewerActivity.java

--- PdfViewerActivity.java
+++ PdfViewerActivity.java
@@ -1,18 +1,21 @@
 package com.example.internlink;
 
 import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PdfViewerActivity extends AppCompatActivity {
 
     private WebView webView;
+    private static final String TAG = "PdfViewerActivity";
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -28,17 +31,37 @@ public class PdfViewerActivity extends AppCompatActivity {
             return;
         }
 
-        // Google Docs Viewer wrapped around the Dropbox PDF URL
-        String googleViewerUrl = "https://docs.google.com/gview?embedded=true&url=" + pdfUrl;
+        Log.d(TAG, "Attempting to load PDF URL: " + pdfUrl);
+
+        // Encode the URL properly
+        String encodedPdfUrl = Uri.encode(pdfUrl);
+        String googleViewerUrl = "https://docs.google.com/gview?embedded=true&url=" + encodedPdfUrl;
+
+        Log.d(TAG, "Google Viewer URL: " + googleViewerUrl);
 
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setBuiltInZoomControls(true);
         settings.setDisplayZoomControls(false);
+        settings.setDomStorageEnabled(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
 
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Log.d(TAG, "Page finished loading: " + url);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Log.e(TAG, "WebView error: " + description);
+                Toast.makeText(PdfViewerActivity.this, "Error loading PDF: " + description, Toast.LENGTH_LONG).show();
+            }
+        });
+
         webView.setWebChromeClient(new WebChromeClient());
-
         webView.loadUrl(googleViewerUrl);
     }
 
